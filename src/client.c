f94852776738ba8976ff04bb3aec078488283aa7
Luciano Almeida <mdiebr@gmail.com>
2016-07-06 12:58:06 -0300

    Arruma makefile e integração com GUI
    Signed-off-by: Luciano Almeida <mdiebr@gmail.com>

--- src/client.c
+++ src/client.c
@@ -4,8 +4,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <resolv.h>
+#include <arpa/inet.h>
 
-void reset_position ()
+#define MY_PORT 4006
+#define MAXBUF 128
+
+char* positions[14];
+
+void* reset_position ()
 {
 	int i;
 	struct uart_data dados = {"C", 1};
@@ -43,7 +53,7 @@ void move_arm (float end_pos[3], FILE* logs[3])
 		sprintf (dados.data, "A%03d%03d%03d", it[0], it[1], it[2]);
 		uart_send (&dados);
 
-		usleep (300e3);
+		usleep (100e3);
 
 		float new_position[3], new_pos_xyz[3];
 		end_position (new_position, arm_len, cur_angle);
@@ -64,37 +74,71 @@ int main (int argc, char* argv[])
 	fp_log[0] = fopen("./logs/trajectory", "w");
 	fp_log[1] = fopen("./logs/angles", "w");
 	fp_log[2] = fopen("./logs/norms", "w");
-
 	if ((errno = uart_init ((argc < 2) ? "/dev/null" : argv[1])) < 0) {
 		fprintf (stderr, "uart_init: initialization error.\n");
-		exit(1);
+		return(1);
 	}
 
-	int i;
-	char line[128];
-	float end_position[3];
+	int sockfd;
+	struct sockaddr_in self;
+	char buffer[MAXBUF];
 
+	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
+	{
+		perror("Socket");
+		exit(errno);
+	}
+
+	bzero(&self, sizeof(self));
+	self.sin_family = AF_INET;
+	self.sin_port = htons(MY_PORT);
+	self.sin_addr.s_addr = INADDR_ANY;
+	int i;
+	float end_position[3];
 	reset_position ();
 
-	while (fgets (line, 128, stdin) != NULL)
+	if ( bind(sockfd, (struct sockaddr*)&self, sizeof(self)) != 0 )
 	{
-		switch (line[0])
+		perror("socket--bind");
+		exit(errno);
+	}
+
+	if ( listen(sockfd, 1) != 0 )
+	{
+		perror("socket--listen");
+		exit(errno);
+	}
+	int clientfd;
+	struct sockaddr_in client_addr;
+	int addrlen=sizeof(client_addr);
+
+	clientfd = accept(sockfd, (struct sockaddr*)&client_addr, &addrlen);
+	printf("%s:%d connected\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+	while (recv(clientfd, buffer, MAXBUF-1, 0) > 0)
+	{
+		switch (buffer[0])
 		{
 			case 'A':
-			sscanf (line + 1, "%f %f %f", &end_position[0], &end_position[1], &end_position[2]);
-			move_arm (end_position, fp_log);
+				sscanf (buffer + 1, "%f %f %f%*c", &end_position[0], &end_position[1], &end_position[2]);
+				move_arm (end_position, fp_log);
+				printf("Done.\n");
 			break;
 			case 'B':
-			reset_position ();
+				reset_position ();
+				printf("Done.\n");
 			break;
 			default:
-			;
+			break;
 		}
 	}
+	
+	printf("Client disconnected... \n");
+	close(clientfd);
+	close(sockfd);
 
 	for (i = 0; i < 3; i++)
 		fclose (fp_log[i]);
-
+	
 	uart_end ();
 
 	return 0;
